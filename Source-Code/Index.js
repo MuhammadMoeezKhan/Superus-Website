e4df045fb71914f27621cc35edec5966c9cf1295
Muhammad Moeez Khan <85444389+MuhammadMoeezKhan@users.noreply.github.com>
2021-10-12 01:02:11 -0400

    Add files via upload

---
+++ Source-Code/Index.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".typing", {
+    strings:["#FreshOutOfTheGame"],
+    typeSpeed:90,
+    reset:true
+    });
+    
+    const sr = ScrollReveal({
+        origin: 'left',
+        distance: '80px',
+        duration: 1200,
+        reset: true
+    });
+    
+    /*SCROLL HOME*/
+    sr.reveal('.title',{delay:2400}); 
+     
